012bc8e665b51bebf46f28bda093b0355bca4f21
DiplineRU <artgul52@gmail.com>
2024-09-09 16:47:08 +0300

    initial commit

---
+++ src/main/java/com/example/Collectionlistset/CollectionListSetApplication.java
@@ -0,0 +1,13 @@
+package com.example.Collectionlistset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionListSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionListSetApplication.class, args);
+	}
+
+}
